3048c8b2d0eb415e1ffae672362dc807c272844b
Ruyther Costa <ruy_fusion@hotmail.com>
2024-06-19 20:17:35 -0600

    Initial Commit

---
+++ NetworkMonitor/NetworkMonitor.h
@@ -0,0 +1,18 @@
+//
+//  NetworkMonitor.h
+//  NetworkMonitor
+//
+//  Created by Ruyther Costa on 2024-06-19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkMonitor.
+FOUNDATION_EXPORT double NetworkMonitorVersionNumber;
+
+//! Project version string for NetworkMonitor.
+FOUNDATION_EXPORT const unsigned char NetworkMonitorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkMonitor/PublicHeader.h>
+
+
