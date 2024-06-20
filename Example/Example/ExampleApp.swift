8b95d8bc70038d6f5b23aa175ab7076cf9227925
Ruyther Costa <ruy_fusion@hotmail.com>
2024-06-19 20:56:09 -0600

    Creating workspace file to arrange framework and example project on only one place

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Ruyther Costa on 2024-06-19.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- NetworkMonitor/NetworkMonitor.h
+++ NetworkMonitor/NetworkMonitor.h
@@ -14,5 +14,3 @@ FOUNDATION_EXPORT double NetworkMonitorVersionNumber;
 FOUNDATION_EXPORT const unsigned char NetworkMonitorVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <NetworkMonitor/PublicHeader.h>
-
-
